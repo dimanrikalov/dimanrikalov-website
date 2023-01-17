ce7979e949d260632c67d1491cc45801a0c9cd3a
Diman Rikalov <dimanrikalov1@gmail.com>
2023-01-17 17:29:12 +0200

    fixed spacings

--- public/detectBrowser.js
+++ public/detectBrowser.js
@@ -10,14 +10,18 @@ link.rel = 'stylesheet';
     
     if(userAgent.match(/chrome|chromium|crios/i)){
         browserName = "chrome";
-        link.href = './safariMediaQueries.css';
+        link.href = './chromeMediaQueries.css';
         document.getElementsByTagName('HEAD')[0].appendChild(link);
+        console.log(browserName);
+        return;  
       }else if(userAgent.match(/firefox|fxios/i)){
         browserName = "firefox";
       }  else if(userAgent.match(/safari/i)){
         browserName = "safari";
         link.href = './safariMediaQueries.css';
         document.getElementsByTagName('HEAD')[0].appendChild(link);
+        console.log(browserName);
+        return
       }else if(userAgent.match(/opr\//i)){
         browserName = "opera";
       } else if(userAgent.match(/edg/i)){
@@ -26,5 +30,5 @@ link.rel = 'stylesheet';
         browserName="No browser detection";
       }
     
-     console.log(browserName);        
+           
 })();
